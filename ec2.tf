3aec299b9cc2591fde3395d095965d7bc7946628
Divya <divyars2492@gmail.com>
2021-04-01 12:43:45 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "ec2-demo" {
   ami = "ami-0577b787189839998"
   instance_type = "t2.micro"
-  
-  tags = {
-    Name = "tf-example"
-  }
+}
+
+resource "aws_ec2_tag" "example" {
+  resource_id = aws_instance.ec2-demo
+  key         = "Name"
+  value       = "Hello World"
 }
